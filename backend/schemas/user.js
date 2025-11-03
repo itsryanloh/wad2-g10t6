312f6ce705faab7e5614bf25e473f9c3f4dee9e7
Ryan Loh <ryan.loh.2022@scis.smu.edu.sg>
2025-11-03 10:59:51 +0800

    fix: backend user schema allow null gender

--- backend/schemas/user.js
+++ backend/schemas/user.js
@@ -4,7 +4,7 @@ const User = z.object({
   name: z.string(),
   username: z.string().min(3),
   age: z.number(),
-  gender: z.string(),
+  gender: z.string().nullable(),
   password: z.string().min(8),
   contact_no: z.string(),
   avatar_url: z.string(),
