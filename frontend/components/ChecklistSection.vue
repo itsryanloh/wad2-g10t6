525b6241958d42df640d2175e5ce1e0ea9522c00
Zachareee <zacharytanhx@yahoo.com>
2025-11-05 21:45:57 +0800

    Shifted breakpoints down in ChecklistSection

--- frontend/components/ChecklistSection.vue
+++ frontend/components/ChecklistSection.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="col-lg-6 d-flex flex-column">
+  <div class="col-md-6 d-flex flex-column">
     <div class="rounded-top-5 text-light p-1" :style>
       <slot />
     </div>
     <div class="bg-white d-flex justify-content-center p-3 rounded-bottom-5 flex-grow-1">
       <div class="row w-100 h-100">
-        <div v-if="checklists.length" v-for="checklist in checklists" class="col-xl-6">
+        <div v-if="checklists.length" v-for="checklist in checklists" class="col-lg-6">
           <ChecklistEntry v-bind="checklist" />
         </div>
         <div v-else class="d-flex justify-content-center align-items-center fw-medium" style="color: #FF9C0C;">
