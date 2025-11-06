64663117e393cc313f77fb1c3e4fb9bb84e397bd
Zachareee <zacharytanhx@yahoo.com>
2025-11-06 16:50:03 +0800

    Updated checklist index to use icons

--- frontend/components/ChecklistEntry.vue
+++ frontend/components/ChecklistEntry.vue
@@ -1,14 +1,18 @@
 <template>
   <div role="button"
-    class="g-5 border border-3 border-warning rounded-4 w-100 h-100 user-select-none community-item d-flex flex-md-column align-items-center p-2"
+    class="g-5 border border-3 border-warning rounded-4 w-100 user-select-none hover-effect d-flex flex-md-column align-items-center p-2"
     style="transition: all 0.3s;" @click="navigateTo({
       name: 'checklist-postId',
       params: { postId: post_id }
     }
     )">
-    <p class="m-auto fs-3 fw-medium flex-md-grow-0 flex-grow-1 text-center pe-2 pe-md-0" style="color: #FF9C0C;">
-      {{ title }}
-    </p>
+    <div class="my-auto text-center w-100 d-flex gap-3 align-items-center">
+      <img :src="image_url"
+        :style="Object.fromEntries(['minWidth', 'minHeight', 'maxWidth', 'maxHeight'].map(k => [k, image_size]))" />
+      <p class="fs-3 fw-medium m-0 flex-grow-1" style="color: #FF9C0C;">
+        {{ title }}
+      </p>
+    </div>
     <div class="d-flex align-items-center justify-content-center gap-2 status-bar px-2">
       <div class="w-75 justify-content-center d-none d-md-block">
         <div class="rounded-pill" style="height: 10px; background: #FFD9B3;">
@@ -20,19 +24,21 @@
           </div>
         </div>
       </div>
-      <small class="fs-4 fw-normal" style="color: #449d48;">{{ completed_count }}/{{ CHECKLIST_ITEMS
-      }}</small>
+      <small class="fs-4 fw-normal" style="color: #449d48;">
+        {{ completed_count }}/{{ CHECKLIST_ITEMS }}
+      </small>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-const { completed_count } = defineProps<{ title: string, post_id: string, completed_count: number }>()
+const { completed_count } = defineProps<{ title: string, post_id: string, completed_count: number, image_url: string }>()
 const progress = computed(() => completed_count / CHECKLIST_ITEMS)
+const image_size = "2.5rem"
 </script>
 
 <style>
-.community-item:hover {
+.hover-effect:hover {
   background: #FFF8F0;
   border-color: #FFCC80;
   transform: translateX(5px);
--- frontend/pages/checklist/index.vue
+++ frontend/pages/checklist/index.vue
@@ -38,7 +38,9 @@
 </template>
 
 <script setup lang="ts">
-export type CList = Record<"post_id" | "title", string> & { completed_count: number }
+import type { Post } from '~/composables/usePetDashboard';
+
+export type CList = Record<"post_id" | "title" | "image_url", string> & { completed_count: number }
 const loading = ref(true)
 const checklists = ref<CList[]>([])
 const { VITE_BASE_URL: base_url } = import.meta.env;
@@ -65,18 +67,23 @@ async function loadUserData() {
     }
 
     checklists.value = await (response.json() as (Promise<
-      { posts: { title: string }, post_id: string, item_index: number }[]
+      { posts: Post, post_id: string, item_index: number }[]
     >))
       .then(arr => arr
         .reduce(
-          (accum, { post_id, item_index, posts: { title } }) => {
+          (accum, { post_id, item_index, posts: { title, image_urls } }) => {
             const entry = accum.find(({ post_id: id }) => id === post_id)
             if (entry) entry.completed_set.add(item_index)
-            else accum.push({ post_id, title, completed_set: new Set([item_index]) })
+            else accum.push({
+              post_id,
+              title,
+              image_url: image_urls?.[0] || '/uniform_cat1.png',
+              completed_set: new Set([item_index])
+            })
             return accum
           }, [] as (Omit<CList, "completed_count"> & { completed_set: Set<number> })[]
         )
-        .map(({ title, post_id, completed_set }) => ({ title, post_id, completed_count: completed_set.size }))
+        .map(({ title, post_id, image_url, completed_set }) => ({ title, post_id, image_url, completed_count: completed_set.size }))
       )
     loading.value = false
   } catch (err) {
